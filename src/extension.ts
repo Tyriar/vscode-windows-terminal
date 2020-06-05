bc58e501b7c83ea054bab008aa037283389adc5e
Daniel Imms <tyriar@tyriar.com>
2020-06-05 11:22:16 -0700

    Fix formatting

--- src/extension.ts
+++ src/extension.ts
@@ -7,58 +7,58 @@ import { promisify } from 'util';
 import { dirname } from 'path';
 
 interface ILaunchWithProfileArgs {
-	authority?: string;
-	fsPath?: string;
-	path?: string;
+  authority?: string;
+  fsPath?: string;
+  path?: string;
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('vscode-windows-terminal.openWithProfile', e => openWithProfile(e)));
+  context.subscriptions.push(vscode.commands.registerCommand('vscode-windows-terminal.openWithProfile', e => openWithProfile(e)));
 }
 
-export function deactivate() {}
+export function deactivate() { }
 
 async function openWithProfile(e?: ILaunchWithProfileArgs) {
-	try {
-		const settings = await getWTSettings();
-		const quickPickItems: (vscode.QuickPickItem & { profile: IWTProfile })[] = settings.profiles.list.map(profile => {
-			const isDefault = profile.guid === settings.defaultProfile;
-			return {
-				label: profile.name,
-				description: (profile.commandline || profile.source || '') + (isDefault ? ' (Default)' : ''),
-				profile
-			};
-		});
+  try {
+    const settings = await getWTSettings();
+    const quickPickItems: (vscode.QuickPickItem & { profile: IWTProfile })[] = settings.profiles.list.map(profile => {
+      const isDefault = profile.guid === settings.defaultProfile;
+      return {
+        label: profile.name,
+        description: (profile.commandline || profile.source || '') + (isDefault ? ' (Default)' : ''),
+        profile
+      };
+    });
 
-		const item = await vscode.window.showQuickPick(quickPickItems);
-		if (!item) {
-			return;
-		}
+    const item = await vscode.window.showQuickPick(quickPickItems);
+    if (!item) {
+      return;
+    }
 
-		const args = ['-p', item.profile.name];
-		if (e?.fsPath) {
-			let cwd = e.fsPath;
-			if (e.authority) {
-				if (!e.path) {
-					throw new Error('authority set but not path');
-				}
-				if (e.authority.startsWith('wsl+')) {
-					const distro = e.authority.split('+')[1];
-					cwd = await toWindowsPath(e.path!, distro);
-				} else {
-					throw new Error(`Unsupported authority "${e.authority}`);
-				}
-			}
+    const args = ['-p', item.profile.name];
+    if (e?.fsPath) {
+      let cwd = e.fsPath;
+      if (e.authority) {
+        if (!e.path) {
+          throw new Error('authority set but not path');
+        }
+        if (e.authority.startsWith('wsl+')) {
+          const distro = e.authority.split('+')[1];
+          cwd = await toWindowsPath(e.path!, distro);
+        } else {
+          throw new Error(`Unsupported authority "${e.authority}`);
+        }
+      }
       if (await isFile(cwd)) {
         cwd = dirname(cwd);
       }
-			args.push('-d', cwd);
-		}
+      args.push('-d', cwd);
+    }
 
-		spawn('wt.exe', args, { detached: true });
-	} catch (ex) {
-		return vscode.window.showErrorMessage(`Could not launch Windows Terminal:\n\n${ex.message}`);
-	}
+    spawn('wt.exe', args, { detached: true });
+  } catch (ex) {
+    return vscode.window.showErrorMessage(`Could not launch Windows Terminal:\n\n${ex.message}`);
+  }
 }
 
 async function isFile(path: string): Promise<boolean> {
--- src/wsl.ts
+++ src/wsl.ts
@@ -1,12 +1,12 @@
 import { exec } from "child_process";
 
 export async function toWindowsPath(p: string, distro: string): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-        exec(`wsl.exe -d ${distro} wslpath -w ${p}`, (err, stdout, stderr) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(stdout.trim());
-        });
+  return new Promise<string>((resolve, reject) => {
+    exec(`wsl.exe -d ${distro} wslpath -w ${p}`, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(stdout.trim());
     });
+  });
 }
