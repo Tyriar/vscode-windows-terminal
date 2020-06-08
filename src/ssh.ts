f05bc3e44f1042e2f9776f955d389a5ca9907a77
ricar <ricar@ASUS>
2020-06-08 14:07:10 +0100

    use promisify and try to reduce nesting

--- src/ssh.ts
+++ src/ssh.ts
@@ -1,5 +1,6 @@
-import { readFile } from "fs";
+import { readFile, exists } from 'fs';
 import * as vscode from "vscode";
+import { promisify } from 'util';
 const SSHConfig = require("ssh-config");
 
 interface IParam {
@@ -8,42 +9,45 @@ interface IParam {
   config: Array<IParam>;
 }
 
-export function resolveSSHHostName(host: string) {
-  return new Promise<any | undefined>(function (resolve, reject) {
-    // get path from remote-ssh-extension config file
-    const remoteSettingsPath: string | undefined = vscode.workspace
-      .getConfiguration("remote.SSH")
-      .get("configFile");
-    if (remoteSettingsPath) {
-      readFile(remoteSettingsPath, function (err, contents) {
-        if (!err) {
-          let resolvedHost = "";
-          // parse file
-          const config: Array<IParam> = SSHConfig.parse(contents.toString());
-          const settings = config.find((x) => x.param.toLowerCase() === "host" && x.value === host);
+export async function resolveSSHHostName(host: string): Promise<string> {
+  // get path from remote-ssh-extension config file
+  const remoteSettingsPath: string | undefined = vscode.workspace
+    .getConfiguration("remote.SSH")
+    .get("configFile");
 
-          if (settings) {
-            const hostname = settings.config.find((x) => x.param.toLowerCase() === "hostname");
-            if (hostname && hostname.value) {
-              resolvedHost += `${hostname.value}`;
-            }
-            const user = settings.config.find((x) => x.param.toLowerCase() === "user");
-            if (user && user.value && hostname?.value) {
-              resolvedHost = `${user.value}@` + resolvedHost;
-            }
-          }
+  if (!remoteSettingsPath) {
+    return host;
+  }
 
-          if (resolvedHost === "") {
-            resolvedHost = host;
-          }
+  const pathExists = await promisify(exists)(remoteSettingsPath);
+  if (!pathExists) {
+    throw Error('Expected remote ssh settings file does not exist: ' + remoteSettingsPath);
+  }
 
-          return resolve(resolvedHost);
-        } else {
-          return resolve(host);
-        }
-      });
-    } else {
-      return resolve(host);
+  const rawString = (await promisify(readFile)(remoteSettingsPath))?.toString();
+  if (!rawString) {
+    throw Error('Could not read remote ssh settings file');
+  }
+
+  // parse content
+  const config: Array<IParam> = SSHConfig.parse(rawString);
+  const settings = config.find((x) => x.param.toLowerCase() === "host" && x.value === host);
+
+  let resolvedHost = "";
+  if (settings) {
+    const hostname = settings.config.find((x) => x.param.toLowerCase() === "hostname");
+    if (hostname && hostname.value) {
+      resolvedHost += `${hostname.value}`;
     }
-  });
+    const user = settings.config.find((x) => x.param.toLowerCase() === "user");
+    if (user && user.value && hostname?.value) {
+      resolvedHost = `${user.value}@` + resolvedHost;
+    }
+  }
+
+  if (resolvedHost === "") {
+    resolvedHost = host;
+  }
+
+  return resolvedHost;
 }
