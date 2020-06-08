66120e34cc229e2978438cc7a0c3f0e635f1374a
Daniel Imms <tyriar@tyriar.com>
2020-06-08 06:17:59 -0700

    Lint code

--- src/ssh.ts
+++ src/ssh.ts
@@ -1,19 +1,19 @@
 import { readFile, exists } from 'fs';
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 import { promisify } from 'util';
-const SSHConfig = require("ssh-config");
+const sshConfig = require('ssh-config');
 
 interface IParam {
   param: string;
   value: string;
-  config: Array<IParam>;
+  config: IParam[];
 }
 
 export async function resolveSSHHostName(host: string): Promise<string> {
   // get path from remote-ssh-extension config file
   const remoteSettingsPath: string | undefined = vscode.workspace
-    .getConfiguration("remote.SSH")
-    .get("configFile");
+    .getConfiguration('remote.SSH')
+    .get('configFile');
 
   if (!remoteSettingsPath) {
     return host;
@@ -30,22 +30,22 @@ export async function resolveSSHHostName(host: string): Promise<string> {
   }
 
   // parse content
-  const config: Array<IParam> = SSHConfig.parse(rawString);
-  const settings = config.find((x) => x.param.toLowerCase() === "host" && x.value === host);
+  const config: IParam[] = sshConfig.parse(rawString);
+  const settings = config.find((x) => x.param.toLowerCase() === 'host' && x.value === host);
 
-  let resolvedHost = "";
+  let resolvedHost = '';
   if (settings) {
-    const hostname = settings.config.find((x) => x.param.toLowerCase() === "hostname");
+    const hostname = settings.config.find((x) => x.param.toLowerCase() === 'hostname');
     if (hostname && hostname.value) {
       resolvedHost += `${hostname.value}`;
     }
-    const user = settings.config.find((x) => x.param.toLowerCase() === "user");
+    const user = settings.config.find((x) => x.param.toLowerCase() === 'user');
     if (user && user.value && hostname?.value) {
       resolvedHost = `${user.value}@` + resolvedHost;
     }
   }
 
-  if (resolvedHost === "") {
+  if (resolvedHost === '') {
     resolvedHost = host;
   }
 
